52054d9e08169a9d4be7b5fc9e0b2b5d128386e9
Russ Yatsenko <ryatsenko@csus.edu>
2019-10-02 16:36:06 -0700

    Updated .js file

---
+++ ryscript.js
@@ -0,0 +1,78 @@
+
+var GoogleAuth;
+var SCOPE = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+function handleClientLoad() {
+  // Load the API's client and auth2 modules.
+  // Call the initClient function after the modules load.
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+  // Retrieve the discovery document for version 3 of Google Drive API.
+  // In practice, your app can retrieve one or more discovery documents.
+  var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+
+  // Initialize the gapi.client object, which app uses to make API requests.
+  // Get API key and client ID from API Console.
+  // 'scope' field specifies space-delimited list of access scopes.
+  gapi.client.init({
+      'apiKey': 'AIzaSyAsrxRS0h5Rzh7mUBZkF6JY1lMCbeuwD8o',
+      'discoveryDocs': [discoveryUrl],
+      'clientId': '82852357315-797ea3r5oervp1o34jlng08b19vm6v54.apps.googleusercontent.com',
+      'scope': SCOPE
+  }).then(function () {
+    GoogleAuth = gapi.auth2.getAuthInstance();
+
+    // Listen for sign-in state changes.
+    GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+    // Handle initial sign-in state. (Determine if user is already signed in.)
+    var user = GoogleAuth.currentUser.get();
+    setSigninStatus();
+
+    // Call handleAuthClick function when user clicks on
+    //      "Sign In/Authorize" button.
+    $('#sign-in-or-out-button').click(function() {
+      handleAuthClick();
+    }); 
+    $('#revoke-access-button').click(function() {
+      revokeAccess();
+    }); 
+  });
+}
+
+function handleAuthClick() {
+  if (GoogleAuth.isSignedIn.get()) {
+    // User is authorized and has clicked 'Sign out' button.
+    GoogleAuth.signOut();
+  } else {
+    // User is not signed in. Start Google auth flow.
+    GoogleAuth.signIn();
+  }
+}
+
+function revokeAccess() {
+  GoogleAuth.disconnect();
+}
+
+function setSigninStatus(isSignedIn) {
+  var user = GoogleAuth.currentUser.get();
+  var isAuthorized = user.hasGrantedScopes(SCOPE);
+  if (isAuthorized) {
+    $('#sign-in-or-out-button').html('Sign out');
+    $('#sign-in-or-out-button').css('display', 'none');
+    $('#revoke-access-button').css('display', 'none');
+    $('#auth-status').html('$15,000.00');
+    $('#auth-status').css('font-size: 18px');
+  } else {
+    $('#sign-in-or-out-button').html('Sign In');
+    $('#revoke-access-button').css('display', 'none');
+    $('#auth-status').html('You have not authorized this app or you are ' +
+        'signed out.' + 
+        '\nSign In to see the price.');
+  }
+}
+
+function updateSigninStatus(isSignedIn) {
+  setSigninStatus();
+}
